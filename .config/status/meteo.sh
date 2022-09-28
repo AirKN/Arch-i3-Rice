c47b6af3c76434b057e9c5d6e5d7078a32a63f57
AirKN <o2controltechpas@protonmail.com>
2022-09-28 19:01:02 +0100

    fixed weather status, everything is perfect

--- .config/status/meteo.sh
+++ .config/status/meteo.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 
-echo -n ☔$(cat ~/.weatherreport | sed -n 16p | sed -e 's/^[[^m]*m//g' | grep -o "[0-9]*%" | sort -n | sed -e '$!d')
+echo -n ☔$(cat ~/.weatherreport | sed -n 14p | sed -e 's/^[[^m]*m//g' | grep -o "[0-9]*%" | sort -n | sed -e '$!d')
 
 echo -n ' '
 
-#echo -n $(cat ~/.weatherreport | sed -n 13p | sed -e 's/^[[^m]*m//g;s/[0-9]/ /g;s/ /\n/g;/^s*$/d' | grep [0-9] | sort -n | sed -e 1b -e '$!d' | tr '\n' ' ' | awk '{print " ❄️",$1 "°"," ",$2 "°"}')
+echo -n $(cat ~/.weatherreport | sed -n 11p| sed -e 's/^[[^m]*m//g;s/ /\n/g;s/°C//g;/^s*$/d' | grep -o "[0-9]*" | sort -n | sed -e 1b -e '$!d'| tr '\n' ' ' | awk '{print " ❄",$1 "°"," ☀",$2 "°"}')
 
-echo -n $(cat ~/.weatherreport | sed -n 13p| sed -e 's/^[[^m]*m//g;s/ /\n/g;s/°C//g;/^s*$/d' | grep -o "+[0-9]*" | sort -n | sed -e 1b -e '$!d' | tr '\n' ' ' | awk '{print " ❄",$1 "°"," ☀️",$2 "°"}')
